3315e9b0e2e9898d233d20922a3a4b166425271f
Andrew Johnson <helios2k6@gmail.com>
2014-04-30 04:46:55 +0100

    adding reconfiguration actor. removing stupid output actor

--- Multivolume Backup Tool/BackupActor.fs
+++ Multivolume Backup Tool/BackupActor.fs
@@ -4,13 +4,23 @@ open System
 open Microsoft.FSharp.Collections
 
 ///<summary>The messages you can send the backup actor</summary>
-type BackupMessage = Start
+type BackupMessage = 
+   | Start
+   | SetArchiveActor of IActor
+
+
+type BackupActorState = { FilesBackedUp : Set<String>; BackupActor : IActor option }
 
 type BackupActor() =
-   inherit ActorBase<BackupMessage, Set<String>>()
+   inherit ActorBase<BackupMessage, BackupActorState>()
+
+   (* Private Methods *)
+   member private this.BackupFiles state = state
 
    (* Public Methods *)
    override this.Receive sender msg state =
-      state
+      match msg with 
+      | SetArchiveActor(actor) -> { state with BackupActor = Some(actor) }
+      | Start -> this.BackupFiles state
 
-   override this.PreStart() = Set.empty
+   override this.PreStart() = { FilesBackedUp = Set.empty; BackupActor = None }
